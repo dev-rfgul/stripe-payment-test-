bc4dc070588e624084da0ad746868c7213f605ad
dev-rfgul <rfgul587@gmail.com>
2024-12-02 00:15:12 +0500

    uploading the project

---
+++ server.js
@@ -0,0 +1,29 @@
+// This is your test secret API key.
+// Replace it with your live secret API key in production.
+const secretKey = process.env.REACT_APP_SECRET_KEY;
+const stripe = require('stripe')(`${secretKey}`);
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: 'price_1QQxWVRqZfeqMphlUFyssq9O',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    automatic_tax: { enabled: true },
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
