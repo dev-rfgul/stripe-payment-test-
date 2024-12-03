e3be8394185441cb75eb4701fe9e7d70cca9e461
dev-rfgul <rfgul587@gmail.com>
2024-12-04 00:03:16 +0500

    the card payemnet is working fully fine now and google pay is to be added,till here teh google option is enabled but its not being shown in the checkout page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // This is your test secret API key.
 // Replace it with your live secret API key in production.
-const secretKey = process.env.REACT_APP_SECRET_KEY;
+// const secretKey = process.env.REACT_APP_SECRET_KEY;
+
+require('dotenv').config();
+const secretKey = process.env.STRIPE_SECRET_KEY;
 const stripe = require('stripe')(`${secretKey}`);
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+console.log(secretKey)
 
 const YOUR_DOMAIN = 'http://localhost:4242';
 
@@ -13,7 +17,7 @@ app.post('/create-checkout-session', async (req, res) => {
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1QQxWVRqZfeqMphlUFyssq9O',
+        price: 'price_1QS19FRqZfeqMphlQRhfY2Oc',
         quantity: 1,
       },
     ],
@@ -25,5 +29,5 @@ app.post('/create-checkout-session', async (req, res) => {
 
   res.redirect(303, session.url);
 });
-
 app.listen(4242, () => console.log('Running on port 4242'));
+
